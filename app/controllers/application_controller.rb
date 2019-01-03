acfcc48ef66b67652d7af85ec4990a8c43931eec
ujihisa <ujihisa@gmail.com>
2019-01-03 17:40:16 +0900

    rails g sorcery:install user_activation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
   def index
-    render html: "#{RUBY_DESCRIPTION}"
+    render html: "#{RUBY_DESCRIPTION}, #{current_user.inspect}"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_03_083452) do
+ActiveRecord::Schema.define(version: 2019_01_03_083955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 2019_01_03_083452) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "activation_state"
+    t.string "activation_token"
+    t.datetime "activation_token_expires_at"
+    t.index ["activation_token"], name: "index_users_on_activation_token"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
